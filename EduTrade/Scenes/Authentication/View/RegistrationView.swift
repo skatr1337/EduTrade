8bc2e9158fc3ead3f9874bbdb606c3f12d525915
Filip Biegaj <filip.biegaj@protonmail.com>
2024-12-08 18:49:47 +0100

    project structure cleanup

---
+++ EduTrade/Scenes/Authentication/View/RegistrationView.swift
@@ -0,0 +1,118 @@
+//
+//  RegistrationView.swift
+//  EduTrade
+//
+//  Created by Filip Biegaj on 01/12/2024.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var email = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var viewModel: AuthViewModel
+    
+    var body: some View {
+        VStack {
+            Image("Bitcoin")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 120)
+                .padding(.vertical, 32)
+            
+            VStack(spacing: 24) {
+                InputView(text: $email,
+                          title: "Email Address",
+                          placeholder: "name@example.com")
+                .autocapitalization(.none)
+                
+                InputView(text: $fullname,
+                          title: "Full Name",
+                          placeholder: "Enter your name")
+                
+                InputView(text: $password,
+                          title: "Password",
+                          placeholder: "Enter your Password", isSecureField: true)
+                
+                ZStack(alignment: .trailing) {
+                    InputView(text: $confirmPassword,
+                              title: "Confirm password",
+                              placeholder: "Confirm your Password", isSecureField: true)
+                    
+                    if !password.isEmpty && !confirmPassword.isEmpty {
+                        if password == confirmPassword {
+                            Image(systemName: "checkmark.circle.fill")
+                                .imageScale(.large)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(.systemGreen))
+                        } else {
+                            Image(systemName: "xmark.circle.fill")
+                                .imageScale(.large)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(.systemRed))
+                        }
+                    
+                    }
+                }
+            
+            }
+            
+            .padding(.horizontal)
+            .padding(.top, 12)
+            
+            Button {
+                Task {
+                    try await viewModel.createUser(withEmail: email,
+                        password: password,
+                        fullname: fullname)
+                }
+            } label: {
+                HStack {
+                    Text("SIGN UP")
+                        .fontWeight(.semibold)
+                    Image(systemName: "arrow.right")
+                }
+                .foregroundColor(.white)
+                .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+            }
+            .background(Color(.systemBlue))
+            .disabled(!formIsValid)
+            .opacity(formIsValid ? 1.0 : 0.5)
+            .cornerRadius(10)
+            .padding(.top, 24)
+            
+            Spacer()
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack(spacing: 3) {
+                    Text("Already have an account?")
+                    Text("Sign in")
+                        .fontWeight(.bold)
+                }
+                .font(.system(size: 14))
+            }
+        }
+    }
+}
+
+extension RegistrationView: AuthenticationForProtocol {
+    var formIsValid: Bool {
+        return !email.isEmpty
+        && email.contains("@")
+        && !password.isEmpty
+        && password.count > 5
+        && confirmPassword == password
+        && !fullname.isEmpty
+    }
+    
+    
+}
+
+#Preview {
+    RegistrationView()
+}
